895c7f99c576bb4baeeee57443bdbd7f61403648
woukl22 <woukl22@gmail.com>
2024-07-15 16:08:59 +0900

    Feat: Add accordion UI

--- src/layout/LeftSideBar/index.jsx
+++ src/layout/LeftSideBar/index.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './style.module.css';
 
 function LeftSideBar(){
-    
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setExpanded(!expanded);
+    };
+
     return (
         <div className={style.LeftSideBar}>
-            <div className={style.UserInputContainer}>
-                <div className={style.Container}>
-                    
+            <div className={`${style.UserInputContainer} ${expanded ? style.expanded : ''}`}>
+                <div className='Container'>
+                    <div className='ContainerHeader'>
+                        <span className='ContainerTitle'>User Input</span>
+                        <button className={style.expandButton} onClick={toggleExpand}>
+                            {expanded ? '∧' : '∨'}
+                        </button>
+                    </div>
+                    {expanded && (
+                        <div className={style.UserInputContent}>
+                            <p>This is additional content visible when expanded.</p>
+                        </div>
+                    )}
                 </div>
             </div>
-            {/* <div className={style.FilterContainer}>
-                <div className={style.Container}>
-                    
-                </div>
-            </div> */}
             <div className={style.RankingContainer}>
-                <div className={style.Container}>
+                <div className='Container'>
                     
                 </div>
             </div>
